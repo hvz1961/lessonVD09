3c5cfd7b751c9be1e031000432a53bbfe653a8e2
hvz1961 <vzkhametov@gmail.com>
2025-01-05 19:21:10 +0300

    change code in main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 from app import app, db
 from app.models import User
 
+with app.app_context():
+    db.create_all()
 
-with app.app_context(): # После первого запуска эту строку можно удалить
-    db.create_all() # После первого запуска эту строку можно удалить
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
 
